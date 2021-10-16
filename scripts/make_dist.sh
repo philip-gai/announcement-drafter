865c5fa55ca404f55430a1bd002eb18151cf56d0
Philip Gai <philip-gai@github.com>
2021-10-16 00:10:57 -0500

    Remove non js files from dist

--- scripts/make_dist.sh
+++ scripts/make_dist.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Prequesites to running this script:
+# npm install and npm build have already been run with NODE_ENV not set to production
+
 rm -r dist
 mkdir dist
 cp -r lib dist/
+find dist/lib ! -name "*.js" -type f -delete
 cp package*.json dist/
 cd dist
 npm ci --production
