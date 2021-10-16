601b6817d1d6d95a1803b04cf0b9c9555f27a29f
Philip Gai <philip-gai@github.com>
2021-10-16 03:17:13 -0500

    Once again remove docker stuff

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM node:14-slim
-WORKDIR /usr/src/app
-COPY . .
-RUN npm install
-CMD [ "npm", "start" ]
--- scripts/make_dist.sh
+++ scripts/make_dist.sh
@@ -8,7 +8,6 @@ mkdir dist
 cp -r lib dist/
 find dist/lib ! -name "*.js" -type f -delete
 cp package*.json dist/
-cp Dockerfile dist/
 cp scripts/startup.sh dist/
 cd dist
 npm ci --production
