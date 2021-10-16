5d3889c7e2d9902b535db113e3ca31e9d692a076
Philip Gai <philip-gai@github.com>
2021-10-16 03:06:28 -0500

    Add npm cache clean --force

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -6,6 +6,9 @@ echo "pwd:" $(pwd)
 echo "ls:" $(ls)
 echo "npm config get production:" $(npm config get production)
 
+echo "npm cache clean --force..."
+npm cache clean --force
+
 # https://github.com/projectkudu/kudu/issues/2946#issuecomment-773546205
 echo "npm rebuild..."
 npm rebuild
