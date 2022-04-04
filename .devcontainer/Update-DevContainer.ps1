c54d416a6c654c0a4004f8ba35963b2931d5a0ff
Philip Gai <PhilipMGai@gmail.com>
2022-04-04 16:05:33 +0000

    Fixup Update-DevContainer.ps1

--- .devcontainer/Update-DevContainer.ps1
+++ .devcontainer/Update-DevContainer.ps1
@@ -3,14 +3,23 @@
 # Post devcontainer create script to run
 
 git config --global pull.rebase true
-Set-Location web-app; npm install; Set-Location -
+
+Write-Host "Installing npm dependencies..."
+Set-Location web-app
+npm install
 if (!$?) { exit 1 }
 
-Write-Output "
-# AUTO GENERATED FILE: For testing using the develop app ONLY
+Write-Host "Running npm run build..."
+npm run build
+if (!$?) { exit 1 }
+Set-Location -
+Write-Host "Done."
+
+Write-Host "Generating probot web-app/.env file for testing..."
+Write-Output "# AUTO GENERATED FILE: For testing using the develop app ONLY
 WEBHOOK_PROXY_URL=https://smee.io/B2IbweFaTDUwe8Hg
 APP_ID=144202
-PRIVATE_KEY="$env:PRIVATE_SSH_KEY_DEV"
+PRIVATE_KEY=`"$env:PRIVATE_SSH_KEY_DEV`"
 WEBHOOK_SECRET=$env:WEBHOOK_SECRET_DEV
 GITHUB_CLIENT_ID=Iv1.e91dd7e62b79d5e0
 GITHUB_CLIENT_SECRET=$env:GITHUBAPP_CLIENT_SECRET_DEV
@@ -21,4 +30,5 @@ COSMOS_URI=https://announcement-drafter-cosmos.documents.azure.com:443/
 COSMOS_PRIMARY_KEY=$env:COSMOS_PRIMARY_KEY
 DRY_RUN_COMMENTS=false
 DRY_RUN_POSTS=false
-" >> /workspaces/announcement-drafter/web-app/.env
+" > /workspaces/announcement-drafter/web-app/.env
+Write-Host "Done."
