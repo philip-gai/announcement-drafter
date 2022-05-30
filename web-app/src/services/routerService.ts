eb9c372c5be5ff7c3a25cac0249b3250a64f10da
Philip Gai <PhilipMGai@gmail.com>
2022-05-30 06:30:05 +0000

    Better security and UX
    - Use the state parameter when auth'ing to validate source
    - Update comment wording
    - Show success page before redirecting user
    - Add pull request link to auth url for redirection back to PR
    - Refactor: Extract some commonly used variables

--- web-app/src/services/routerService.ts
+++ web-app/src/services/routerService.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { AppConfig } from "../models/appConfig";
 import { TokenService } from "./tokenService";
 import { AuthService } from "./authService";
+import CryptoJS from "crypto-js";
 
 export class RouterService {
   private _router: Router;
@@ -10,6 +11,8 @@ export class RouterService {
   private _appConfig: AppConfig;
   private _tokenService: TokenService;
   private _authService: AuthService;
+  static readonly SECONDS_TO_REDIRECT = 6;
+  static readonly DEFAULT_REDIRECT = "https://github.com/philip-gai/announcement-drafter";
 
   private constructor(
     logger: DeprecatedLogger,
@@ -47,16 +50,63 @@ export class RouterService {
   }
 
   public addOAuthCallbackRoute(): this {
-    this._router.get(this._appConfig.github_callback_url, async (req, res) => {
+    this._router.get(this._appConfig.github_callback_url, async (req, res): Promise<void> => {
       this._logger.info(`Received OAuth callback...`);
-      const code = (req.query.code as string) || "Bad code";
-      this._logger.debug(`Code: ${code}`);
+      const code = req.query.code as string;
+      if (!code) {
+        res.status(400).send("No code");
+        return;
+      }
+
+      const encryptedState = req.query.state as string;
+      if (!encryptedState) {
+        res.status(400).send("No state");
+        return;
+      }
+
+      // Redirect to the DEFAULT_REDIRECT if no pull request url is found
+      let redirectUrl = RouterService.DEFAULT_REDIRECT;
+      try {
+        // Decrypt the state and validate it
+        const bytes = CryptoJS.AES.decrypt(encryptedState, this._appConfig.github_client_secret);
+
+        const state = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+        if (state.github_client_id !== this._appConfig.github_client_id) {
+          res.status(400).send("Invalid state");
+          return;
+        }
+        // Get the pull request url from the state for redirection
+        if (state.pull_url) {
+          redirectUrl = state.pull_url;
+        }
+      } catch (error) {
+        res.status(400).send("Unable to parse state");
+        return;
+      }
 
       const { token, refreshToken, refreshTokenExpiresAt } = await this._authService.authenticateUser(code);
 
       await this._tokenService.upsertRefreshToken(token, refreshToken, refreshTokenExpiresAt);
 
-      res.redirect("/");
+      // Display authentication success message and redirect after SECONDS_TO_REDIRECT seconds
+      const redirectLocationText = redirectUrl !== RouterService.DEFAULT_REDIRECT ? "pull request" : "Announcement Drafter repository";
+      res.setHeader("Content-Type", "text/html");
+      res.status(200).send(`
+        <html>
+        <head>
+          <title>announcement-drafter | authorization</title>
+        </head>
+        <body>
+          <div><h2>Success! Now Announcement Drafter can create discussions for you 🚀</h2></div>
+          <div><p>Sending you back to the ${redirectLocationText} in just a few seconds...</p></div>
+        </body>
+        <script>
+          setTimeout(function () {
+            window.location = "${redirectUrl}";
+          }, ${RouterService.SECONDS_TO_REDIRECT * 1000})
+        </script>
+        </html>
+        `);
     });
     return this;
   }
