e69b1c70ed04b94068cc556f610c6863df13770f
Philip Gai <PhilipMGai@gmail.com>
2022-05-31 21:37:58 +0000

    Add logging and remove redundant comments

--- web-app/scripts/make_dist.sh
+++ web-app/scripts/make_dist.sh
@@ -6,19 +6,23 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+echo "Creating dist folder."
 rm -rf dist
 mkdir dist
+
+echo "Copying lib output to dist folder."
 cp -r lib dist/
 
-# Remove all non-js files
+echo "Removing all non-js files."
 find dist/lib ! -name "*.js" -type f -delete
 
+echo "Copying package*.json files to dist folder."
 cp package*.json dist/
 
 # Startup command that will be run as part of container startup
+echo "Copying startup.sh to dist folder."
 cp scripts/startup.sh dist/
 
-# Install node modules in the dist folder
-# devDependencies are still needed here
+echo "Installing the node modules in the dist folder."
 cd dist
 npm ci --production
