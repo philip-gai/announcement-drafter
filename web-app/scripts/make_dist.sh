b375bf6f36f971d34c67f50bf2839f3a08d4859a
Philip Gai <PhilipMGai@gmail.com>
2022-05-31 21:24:57 +0000

    Update make_dist.sh

--- web-app/scripts/make_dist.sh
+++ web-app/scripts/make_dist.sh
@@ -3,13 +3,22 @@
 # Prequisites:
 # npm install and npm build have already been run with NODE_ENV not set to production
 
-rm -r dist
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+rm -rf dist
 mkdir dist
 cp -r lib dist/
+
+# Remove all non-js files
 find dist/lib ! -name "*.js" -type f -delete
+
 cp package*.json dist/
+
+# Startup command that will be run as part of container startup
 cp scripts/startup.sh dist/
+
+# Install node modules in the dist folder
+# devDependencies are still needed here
 cd dist
 npm ci --production
-npm cache clean --force
-npm install
