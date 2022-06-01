ca7b28b9ca07ea71c57a6bf4bfff590cdeca0362
Philip Gai <PhilipMGai@gmail.com>
2022-06-01 06:54:47 +0000

    Add comments to make_dist.sh

--- web-app/scripts/make_dist.sh
+++ web-app/scripts/make_dist.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Prequisites:
-# npm install and npm build have already been run with NODE_ENV not set to production
+# 1. npm install and npm build:dev have already been run with NODE_ENV not set to production
+# NOTE: No need to run npm install, this is handled by Oryx build when SCM_DO_BUILD_DURING_DEPLOYMENT is set to true
+# https://github.com/microsoft/Oryx/blob/main/doc/runtimes/nodejs.md#build
 
 # Exit immediately if a command exits with a non-zero status.
 set -e
